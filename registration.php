9ce10376e1d9c44dd96fe370c68b7b8ce233e40f
msaaad <saad99u@gmail.com>
2019-08-21 15:37:03 +0500

    project init

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = $_POST['name'];
+$email_registration = $_POST['email_registration'];
+$password_registration = $_POST['password_registration'];
+$contact = $_POST['contact'];
+$option = $_POST['option'];
+
+?>
