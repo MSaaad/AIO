1e36e9fd683ed98d823018a09436b7c392a8d11d
msaaad <saad99u@gmail.com>
2019-08-23 12:55:24 +0500

    php work

--- registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$name = $_POST['name'];
-$email_registration = $_POST['email_registration'];
-$password_registration = $_POST['password_registration'];
-$contact = $_POST['contact'];
-$option = $_POST['option'];
-
-?>
